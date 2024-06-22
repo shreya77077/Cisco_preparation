ba5cfa6857091f53f57f38001d1d94206d0572af
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-06-22 15:56:38 +0530

    init

--- Climbingstairs.cpp
+++ Climbingstairs.cpp
@@ -5,20 +5,40 @@ using namespace std;
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n <= 1) return 1;
+    // int climbStairs(int n) {
+    //     if (n <= 1) return 1;
 
-        vector<int> dp(n + 1);
-        dp[0] = 1; // Base case: 1 way to stay at the ground
-        dp[1] = 1; // Base case: 1 way to reach the first step
+    //     vector<int> dp(n + 1);
+    //     dp[0] = 1; // Base case: 1 way to stay at the ground
+    //     dp[1] = 1; // Base case: 1 way to reach the first step
 
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
+    //     for (int i = 2; i <= n; ++i) {
+    //         dp[i] = dp[i - 1] + dp[i - 2];
+    //     }
 
-        return dp[n];
+    //     return dp[n];
+    int climbStairs(int n){
+    if (n <=1) return 1;
+    vector<int> dp(n+1);
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i =2; i<=n;i++){
+        dp[i] = dp[i-1]+ dp[i-2];
+    }
+    return dp[n];
+}
     }
 };
+// int climbStairs(int n){
+//     if (n <=1) return 1;
+//     vector<int> dp(n+1);
+//     dp[0] = 1;
+//     dp[1] = 1;
+//     for(int i =2; i<=n;i++){
+//         dp[i] = dp[i-1]+ dp[i-2];
+//     }
+//     return dp[n];
+// }
 
 int main() {
     Solution solution;
