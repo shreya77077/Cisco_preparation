17abd4f5fb3a92f2d67954028cbffd4321ee364d
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-06-27 23:56:19 +0530

    init

--- 2Darray.cpp
+++ 2Darray.cpp
@@ -20,3 +20,20 @@ int main(){
 
     return 0;
 }
+class Solution {
+public:
+    int climbStairs(int n) {
+         if (n <= 1) return 1;
+
+        vector<int> dp(n + 1);
+        dp[0] = 1; // Base case: 1 way to stay at the ground
+        dp[1] = 1; // Base case: 1 way to reach the first step
+
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+        
+    }
+};
